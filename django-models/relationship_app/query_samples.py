9ba32d2ea5f24f704d12eaf7b02c60a82c0f9e0e
kira <kirubelteshome6202@gmail.com>
2024-11-09 20:57:19 +0300

    update

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,15 @@
+from .models import Author, Library
+
+author = Author.objects.get(name="Author Name")
+books_by_author = author.books.all()
+for book in books_by_author:
+    print(book.title)
+
+library = Library.objects.get(name="Library Name")
+books_in_library = library.books.all()
+for book in books_in_library:
+    print(book.title)
+
+library = Library.objects.get(name="Library Name")
+librarian = library.librarian
+print(librarian.name)
