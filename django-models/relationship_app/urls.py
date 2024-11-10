68d728889ac9e78620df922c6bd1ba2633ddbf94
kira <kirubelteshome6202@gmail.com>
2024-11-10 09:55:43 +0300

    update the role based access

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('admin_view/', views.admin_view, name='admin_view'),
-    path('librarian/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
 ]
