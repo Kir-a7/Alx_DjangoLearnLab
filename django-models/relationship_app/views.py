6b2de8b6a91bdf14de71679c89fa52574fa9014d
kira <kirubelteshome6202@gmail.com>
2024-11-10 08:48:51 +0300

    update the views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from relationship_app.models import Book, Library
+from .models import Book, Library
 from django.views.generic import DetailView
 
 def list_books(request):
