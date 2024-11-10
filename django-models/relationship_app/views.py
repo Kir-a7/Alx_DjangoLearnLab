5e2a2a199a29ce84cad47f3ff6e39154971b153a
kira <kirubelteshome6202@gmail.com>
2024-11-10 09:18:46 +0300

    implementing role based access control

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Library
 from relationship_app.models import Book, Library
 from django.views.generic.detail import DetailView
-
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     books = Book.objects.all()  # Get all Book objects
@@ -27,9 +27,6 @@ class LibraryDetailView(DetailView):
         
         return context
 
-
-
-
 # View for user registration
 def register(request):
     if request.method == "POST":
@@ -41,4 +38,29 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# Check functions to verify user role
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
