a63189eb3960bff2bb5b90d73a2fadb269f8d048
kira <kirubelteshome6202@gmail.com>
2024-12-01 21:52:28 +0300

    task-2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,9 +2,10 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticate
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+import django_filters
+
 from .models import Book
 from .serializers import BookSerializer
-import django_filters
 
 
 # Define the filter class for the Book model
