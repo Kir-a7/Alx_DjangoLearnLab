3714ae00d6d3ea8fd099b46f147ce604cfd844f9
kira <kirubelteshome6202@gmail.com>
2024-12-08 20:27:32 +0300

    implementing advance features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,7 +21,10 @@ urlpatterns =[
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='edit_comment'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
     
-    path('search/', views.search_posts, name='search-posts'),  # Search posts
+    path('search/', views.search_posts, name='search-posts'),  
     path('tags/<str:tag_name>/', views.posts_by_tag, name='posts-by-tag'),
- 
+    path('tags/<str:tag_name>/search/', views.posts_by_tag, name='posts_by_tag'),
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts_by_tag'),
+
+   
 ]
