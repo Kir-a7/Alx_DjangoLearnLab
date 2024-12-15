2e6c97af30cfb9e371367ed534143d536f6c35c0
kira <kirubelteshome6202@gmail.com>
2024-12-15 10:58:24 +0300

    implementing user follows and feed functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -19,21 +17,22 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
+       serializer.save(author=self.request.user)
 
 
 class FeedView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
+        
+        followed_users = request.user.following.all()  # Get followed users
+        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')  # Filter posts
+        serializer = PostSerializer(posts, many=True)  # Serialize posts
         return Response(serializer.data, status=status.HTTP_200_OK)
